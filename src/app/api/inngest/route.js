029d5406037ceeccf994a786a7548c09d7b2a6f9
pranjalliik <kaushikpranjali7@gmail.com>
2024-11-22 00:18:26 +0530

    inngest update

--- src/app/api/inngest/route.js
+++ src/app/api/inngest/route.js
@@ -2,9 +2,12 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { createUser, helloWorld ,GenerateNotes ,generateFlashCard , generateQuiz} from "@/inngest/functions";
 
+export const runtime = 'edge' ;
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  streaming: 'allow',
+  
   functions: [
     /* your functions will be passed here later! */
     helloWorld,
